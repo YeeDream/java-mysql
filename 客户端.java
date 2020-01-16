2d39b942136894cf450ba89e82f50c1cc6258eb0
YeeDream <53851133+YeeDream@users.noreply.github.com>
2020-01-16 15:13:30 +0800

    网络通信-客户端

--- 客户端.java
+++ 客户端.java
@@ -1,28 +1,23 @@
-package 复习.网络通信.TCP;
+package 复习.网络通信.$1;
 
-import java.io.IOException;
-import java.io.InputStream;
 import java.net.Socket;
-import java.net.UnknownHostException;
 
 /**
  * @Author DreamYee
- * @Create 2020/01/08  9:43
+ * @Create 2020/01/16  14:45
  */
 public class 客户端 {
     public static void main(String[] args) {
-        try {
-            Socket s = new Socket("localhost", 9999);
+        try{
+            Socket s=new Socket("localhost",9999);
+            输入1 in=new 输入1(s.getInputStream(),"客户端");
+            in.start();
 
-            InputStream in = s.getInputStream();
-            byte[] bytes = new byte[1024];
-            int len = in.read(bytes);
-            String str = new String(bytes, 0, len);
-            System.out.println(str);
-        }catch (UnknownHostException e){
-            System.out.println(e.getMessage());
-        }catch (IOException e){
+            输出1 out=new 输出1(s.getOutputStream(),"服务器");
+            out.start();
+        }catch (Exception e){
             System.out.println(e.getMessage());
         }
+
     }
 }
